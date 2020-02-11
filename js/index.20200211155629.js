e23b75d04f0fe7c669ca4c3379cde03b6f8a7846
zorrodong <dongji5211314@126.com>
2020-02-11 16:00:09 +0800

    New Page

---
+++ js/index.20200211155629.js
@@ -0,0 +1,22 @@
+(function(d){var h=[];d.loadImages=function(a,e){"string"==typeof a&&(a=[a]);for(var f=a.length,g=0,b=0;b<f;b++){var c=document.createElement("img");c.onload=function(){g++;g==f&&d.isFunction(e)&&e()};c.src=a[b];h.push(c)}}})(window.jQuery);
+$.fn.hasAttr = function(name) { var attr = $(this).attr(name); return typeof attr !== typeof undefined && attr !== false; };
+
+
+$(document).ready(function() {
+r=function(){dpi=window.devicePixelRatio;$('.js-6').attr('src', (dpi>1) ? 'images/tu-pian-1-222.jpg' : 'images/tu-pian-1-111.jpg');};
+if(!window.HTMLPictureElement){r();}
+(function(){$('a[href^="#"]:not(.allowConsent,.noConsent,.denyConsent,.removeConsent)').each(function(){$(this).click(function(){var t=this.hash.length>1?$('[name="'+this.hash.slice(1)+'"]').offset().top:0;return $("html, body").animate({scrollTop:t},400),!1})})})();
+$('.cont').Stickyfill();
+var wl = new woolite();
+wl.init();
+wl.addAnimation($('.js')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js-2')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js-3')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js-4')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js-5')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js-7')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js-8')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js-9')[0], "1.00s", "0.00s", 1, 100);
+wl.start();
+
+});
